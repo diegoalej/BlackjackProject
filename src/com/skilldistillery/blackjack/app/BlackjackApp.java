542042993e3b5858790cefdda982c1c10cb82888
Diego Hoyos <hoyosdiegoa@gmail.com>
2020-02-16 14:45:43 -0700

    Added basic menu functionality and error checking

--- src/com/skilldistillery/blackjack/app/BlackjackApp.java
+++ src/com/skilldistillery/blackjack/app/BlackjackApp.java
@@ -18,27 +18,99 @@ public class BlackjackApp {
 		Deck deck = new Deck();
 		BlackjackDealer dealer = new BlackjackDealer(deck);
 		BlackjackPlayer player = new BlackjackPlayer();
+		mainMenu(player, dealer);
+		
+	}
+
+	private void mainMenu(BlackjackPlayer player, BlackjackDealer dealer) {
 		System.out.println("Welcome to Blackjack App ");
-		System.out.println("Let's play a hand ");
-		dealer.dealInitialHand(player);
-		playersTurn(player, dealer);
-		dealersTurn(dealer);
-		determineWinner(player, dealer);
+		int userChoice = 3;
+			while(userChoice != 2) {
+				System.out.println("Enter an integer to select from the menu below: ");
+				System.out.println("1. Play a Blackjack hand ");
+				System.out.print("2. Exit\n>  ");
+			try {
+				userChoice = kb.nextInt();
+				if (userChoice == 1) {
+					while(userChoice != 2) {
+						dealer.dealInitialHand(player);
+						playersTurn(player, dealer);
+						dealersTurn(dealer);
+						determineWinner(player, dealer);
+						System.out.println("there are " + dealer.getDeck().checkDeckSize() + " cards left in the deck" );
+						dealer.getHand().clear();
+						player.getHand().clear();
+						System.out.println("Would you like to play again?");
+						System.out.println("1. Yes! ");
+						System.out.print("2. No\n>  ");
+						try {
+							userChoice = kb.nextInt();
+							if (userChoice == 1) {
+								continue;
+							}
+							else if (userChoice == 2) {
+								System.out.println("Goodbye!");
+								break;
+							}
+							else {
+								System.out.println("Your choice was outside menu bounds, back to main menu");
+								break;
+							}
+						} catch (Exception e) {
+							System.out.println("You did not enter the integers 1 or 2, back to main menu");
+							kb.nextLine();
+							break;
+						}						
+					}
+				}
+				else if (userChoice == 2) {
+					System.out.println("Goodbye!");
+					break;
+				}
+				else {
+					System.out.println("Your choice was outside menu bounds, try again");
+					continue;
+				}
+			} catch (Exception e) {
+				System.out.println("You did not enter the integers 1 or 2, try again");
+				kb.nextLine();
+				continue;
+			}
+		}
 	}
 
 	private void playersTurn(BlackjackPlayer player, BlackjackDealer dealer) {
-		while (!player.getHand().isBust()) {
-			System.out.print("Would you like to \n1) hit  \n2) stay\n>  ");
-			int choice = kb.nextInt();
-			if (choice == 1) {
-				dealer.dealAdditionalCard(player);
-				if (player.getHand().isBust()) {
-					break;
+		if(player.getHand().isBlackjack()) {
+			System.out.println("We do not allow hits on 21, but congratulations on your Blackjack!");
+		}
+		else {
+			while (!player.getHand().isBust()) {
+				System.out.print("Would you like to: \n1) Hit  \n2) Stay\n>  ");
+				try {
+					int choice = kb.nextInt();
+					if (choice == 1) {
+						dealer.dealAdditionalCard(player);
+						if (player.getHand().isBust()) {
+							break;
+						}
+						else if(player.getHand().isBlackjack()) {
+							System.out.println("We do not allow hits on 21, but congratulations on your Blackjack!");
+							break;
+						}
+					} else if (choice == 2) {
+						System.out.println(
+								"You choose to stay with a total of " + player.getHand().getHandValue() + ", dealer's turn");
+						break;
+					}
+					else {
+						System.out.println("Choice outside of menu bounds, try again");
+						continue;
+					}
+				} catch (Exception e) {
+					System.out.println("Incorrect input, try again");
+					kb.nextLine();
+					continue;
 				}
-			} else if (choice == 2) {
-				System.out.println(
-						"You choose to stay with a total of " + player.getHand().getHandValue() + ", dealer's turn");
-				break;
 			}
 		}
 	}
@@ -46,11 +118,12 @@ public class BlackjackApp {
 	private void dealersTurn(BlackjackDealer dealer) {
 		System.out.println(dealer.getClass().getSimpleName() + " has a " + dealer.getHand());
 		while (!dealer.getHand().isBust()) {
-			if (dealer.getHand().getHandValue() <= 17) {
+			if (dealer.getHand().getHandValue() < 17) {
 				dealer.dealAdditionalCard(dealer);
 				if (dealer.getHand().isBust()) {
 					break;
 				}
+				continue;
 			} else
 				System.out.println("Dealer ends turn with a total of " + dealer.getHand().getHandValue());
 			break;
@@ -58,14 +131,14 @@ public class BlackjackApp {
 	}
 
 	private void determineWinner(BlackjackPlayer player, BlackjackDealer dealer) {
-		if (player.getHand().getHandValue() > 21 && dealer.getHand().getHandValue() <= 21) {
+		if (player.getHand().getHandValue() > 21) {
 			System.out.println("House wins!");
 		} else if (dealer.getHand().getHandValue() > 21 && player.getHand().getHandValue() <= 21) {
 			System.out.println("You beat the house!");
 		} else if (player.getHand().getHandValue() <= dealer.getHand().getHandValue()) {
 			System.out.println("House wins!");
 		} else if (player.getHand().getHandValue() > dealer.getHand().getHandValue()) {
-			System.out.println("You bear the house! ");
+			System.out.println("You beat the house! ");
 		}
 
 	}
--- src/com/skilldistillery/blackjack/players/BlackjackDealer.java
+++ src/com/skilldistillery/blackjack/players/BlackjackDealer.java
@@ -13,27 +13,31 @@ public class BlackjackDealer extends BlackjackPlayer {
 		}
 		
 		public void dealInitialHand(BlackjackPlayer player) {
-			System.out.println("Dealing first card to player upside down");
+			System.out.println("Dealing first card to player");
 			player.getHand().addCard(deck.dealCard());
-			System.out.println("You take a peek and see it is a " + player.getHand().getCards().get(0));
-			System.out.println("Dealing first card to dealer upside down");
+			System.out.println("Dealing first card to dealer");
 			this.getHand().addCard(deck.dealCard());
 			
-			System.out.println("Dealing second card to player right-side up");
+			System.out.println("Dealing second card to player");
 			player.getHand().addCard(deck.dealCard());
 			System.out.println("You see it is a " + player.getHand().getCards().get(1));
 			
-			System.out.println("Dealing second card to dealer right-side up");
+			System.out.println("Dealing second card to dealer");
 			this.getHand().addCard(deck.dealCard());
 			System.out.println("You see it is a " + this.getHand().getCards().get(1));
 			
-			System.out.println(player.getClass().getSimpleName() + "you have:\n" + player.getHand());
+			System.out.println(player.getClass().getSimpleName() + " you have a:\n" + player.getHand());
 		}
 		
+		public Deck getDeck() {
+			return deck;
+		}
+
+
 		public void dealAdditionalCard(Player player) {
-			System.out.println("Dealing additional card to " + player.getClass().getSimpleName() );
+			System.out.println("Dealing card to " + player.getClass().getSimpleName() );
 			player.getHand().addCard(deck.dealCard());
-			System.out.println("You see it is a " + player.getHand().getCards().get((player.getHand().getCards().size()-1)));
+			System.out.println("It is a " + player.getHand().getCards().get((player.getHand().getCards().size()-1)));
 			System.out.println(player.getClass().getSimpleName() + " you now have:\n" + player.getHand());				
 			
 		}
